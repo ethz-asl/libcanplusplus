31cce96f63ea3f0b7ba3635139c73aa3e129ce68
menzi <menzi@todo.todo>
2016-01-08 16:57:44 +0100

    Added TxPDONMT message definition

--- libcanplusplus/include/libcanplusplus/canopen_pdos.hpp
+++ libcanplusplus/include/libcanplusplus/canopen_pdos.hpp
@@ -7,7 +7,11 @@
 
 #pragma once
 
+#include <chrono>
+
 namespace canopen {
+
+constexpr int txPDONMT = 0x700;
 
 //////////////////////////////////////////////////////////////////////////////
 class RxPDOSync: public CANOpenMsg {
@@ -18,4 +22,52 @@ public:
   virtual ~RxPDOSync() {};
 };
 
+//////////////////////////////////////////////////////////////////////////////
+class TxPDONMT: public CANOpenMsg {
+public:
+  TxPDONMT(int nodeId, int SMId):CANOpenMsg(txPDONMT+nodeId, SMId),
+  state_(0),
+  timeReceived_()
+  {
+    //0x00 - Bootup; 0x04 - Stopped; 0x05 - Operational; 0x7F - Pre-Operational.
+  };
+
+  virtual ~TxPDONMT()
+  {
+  };
+
+  virtual void processMsg()
+  {
+    timeReceived_ = std::chrono::steady_clock::now();
+    state_ = (uint8_t)(value_[0]);
+  };
+
+  bool isBootup() const
+  {
+    return (state_ & (1<<0x00));
+  };
+
+  bool isStopped() const
+  {
+    return (state_ & (1<<0x04));
+  };
+
+  bool isOperational() const
+  {
+    return (state_ & (1<<0x05));
+  };
+
+  bool isPreOperational() const
+  {
+    return (state_ & (1<<0x7F));
+  };
+
+  const std::chrono::time_point<std::chrono::steady_clock>& getTime() const {
+    return timeReceived_;
+  }
+private:
+  uint8_t state_;
+  std::chrono::time_point<std::chrono::steady_clock> timeReceived_;
+
+};
 } // namespace canopen
